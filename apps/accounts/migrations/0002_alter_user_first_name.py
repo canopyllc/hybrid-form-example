9e362a0976df48787ce9891e851dd127a2b54c8e
Brent O'Connor <epicserve@gmail.com>
2022-09-13 06:48:36 -0500

    Add user model migration

---
+++ apps/accounts/migrations/0002_alter_user_first_name.py
@@ -0,0 +1,16 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("accounts", "0001_initial"),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name="user",
+            name="first_name",
+            field=models.CharField(blank=True, max_length=150, verbose_name="first name"),
+        ),
+    ]
